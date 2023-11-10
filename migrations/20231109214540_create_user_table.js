485d0e76968057ba7fd1d8124ebca0f4bc47283e
Louis Ekpenga <louisekpenga@MBPl20FVFHHBUYQ05D.local>
2023-11-10 10:13:20 -0500

    General set up achieved

---
+++ migrations/20231109214540_create_user_table.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+  .createTable("user", (table) =>{
+    table.increments("id").primary();
+    table.string("firstname").notNullable();
+    table.string("lastname").notNullable(); 
+    table.string("email").notNullable().unique();
+    table.text("profile_pic");
+    table.string("password_hash").notNullable();
+    table.enu("status", ["Active", "Inactive","Deleted"]).defaultTo("Active");  
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")); 
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+   return knex.schema.dropTable("user");
+};
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const nodemon = require("nodemon");
 const app = express(); 
+require("dotenv").config();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5050;
 
 app.get("/", (req, res)=>{
     return res.status(200).json({"message":"Connected"})
 })
 
-app.listen(5050, ()=>{
-    console.log("Server is on!!! 5050")
+app.listen(PORT, ()=>{
+    console.log(`Server is on!!! ${5050}`)
 })
