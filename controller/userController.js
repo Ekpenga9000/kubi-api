cabd545e3e20e424ad3c1347a2d2ad365c47dd37
Louis Ekpenga <louisekpenga@MBPl20FVFHHBUYQ05D.local>
2023-11-22 10:02:25 -0500

    Add Edit User email and name endpoint.

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,11 @@
 const db = require("knex")(require("../knexfile"));
 const {validateToken} = require("../service/jwtService");
 
+const validateEmail = (email) => {
+    const regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    return regex.test(email);
+}
+
 const getUserById = async (req, res) =>{
     const {id} = req.params; 
     const {authorization} = req.headers; 
@@ -40,10 +45,46 @@ const getUserById = async (req, res) =>{
 }
 
 const updateUserDetails = async(req,res)=>{
-    //Check that the body has the data 
-    //Check that there is a token coming
+    const {authorization} = req.headers; 
+    if(!authorization){
+        return res.status(400).json({"message": "Invalid request."}); 
+    }
+    const id = validateToken(authorization, "id"); 
 
-    // compa
+    if(!id){
+        return res.status(400).json({"message": "Invalid request."}); 
+    }
+    const {firstname, lastname, email, profile_pic} = req.body;
+
+    if(!firstname || !lastname || !email || !profile_pic){
+        return res.status(400).json({"messgae":"Invalid request"}); 
+    }
+
+    const userId = await db("user")
+                    .select("id")
+                    .where({id})
+                    .first();
+    
+    if(!userId){
+        return res.status(404).json({"message":"Invalid user."}); 
+    }
+
+    const isEmail = validateEmail(email); 
+
+    if(!isEmail){
+        return res.status(400).json({"message":"invalid email."})
+    }
+    
+    const user = await db("user")
+                .where({ id })
+                .update({
+                    firstname,
+                    lastname,
+                    email,
+                    profile_pic
+                })
+
+    return res.status(200).json({"message":"Account updated!"});
 }
 module.exports = {
     getUserById,
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,5 +2,5 @@ const router = require("express").Router();
 const userController = require("../controller/userController");
 
 router.get("/:id", userController.getUserById);
-router.patch("/", userController.updateUserDetails);
+router.put("/", userController.updateUserDetails);
 module.exports = router;
