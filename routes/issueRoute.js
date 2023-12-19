414b68e84911701c79a5bae297b0d030d55a42d6
Louis_Ekpenga <ekpenga9000@gmail.com>
2023-12-18 21:33:07 -0500

    Create issue endpoint

--- routes/issueRoute.js
+++ routes/issueRoute.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const issueController = require("../controller/issueController");
 
-router.get("/projectId", issueController.fetchIssuesByProjectId);
+router.get("/:projectId", issueController.fetchIssuesByProjectId);
 router.post("/", issueController.createIssue);
 
 module.exports = router;
